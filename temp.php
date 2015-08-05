7719f8c49666295dab377772d5ef086cee50d685
renzosunico <r.sunico@klab.com>
2015-08-05 19:17:18 +0800

    temp.php modified Was changed to only check variable, if null display the form, else compute and display.

--- temp.php
+++ temp.php
@@ -2,22 +2,23 @@
 	<head> <title>Temperature Conversion</title> </head>
 
 	<body>
-	<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') { ?>
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        
+	<?php $fahrenheit = $_GET['fahrenheit'];
+
+    if (is_null($fahrenheit)) { ?>
+        
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
         Fahrenheit temperature:
         <input type="text" name="fahrenheit" /><br />
         <input type="submit" value="Convert to Celcius!" />
     </form>
-    <?php }
-    else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $fahrenheit = $_POST['fahrenheit'];
-        $celsius = ($fahrenheit - 32) * 5 / 9;
         
-        printf("%.2fF is %.2fC", $fahrenheit, $celsius);
-        echo $_SERVER['PHP_SELF'];
-    }
+    <?php }
+
     else {
-        die("This script is only works with GET and POST request");
+        $celsius = ($fahrenheit - 32) * 5 / 9;
+        printf("%.2fF is %.2fC", $fahrenheit, $celsius);
+
     } ?>
 
 
