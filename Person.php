06090f592a85a822eda84cc10e2d661cff664879
renzosunico <r.sunico@klab.com>
2015-08-05 13:36:31 +0800

    Person Class Has name. Can be retrieved and set using the methods.

---
+++ Person.php
@@ -0,0 +1,18 @@
+<?php
+
+class Person
+{
+    public $name = '';
+    
+    function getName()
+    {
+        return $this->name;
+    }
+    
+    function setName($newName)
+    {
+        $this->name = $newName;
+    }
+}
+
+?>
