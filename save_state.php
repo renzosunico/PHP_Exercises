c4dc56989c9c163342eb6aa1329818ad83abb173
renzosunico <r.sunico@klab.com>
2015-08-05 22:04:30 +0800

    Combination of Cookies and Session Saves the background color choice to cookies.

---
+++ save_state.php
@@ -0,0 +1,31 @@
+<?php
+    if($_POST['bgcolor']) {
+        setcookie('bgcolor', $_POST['bgcolor'], time() + (60 * 60 * 24 * 7));
+    }
+    if (isset($_COOKIE['bgcolor'])) {
+        $backgroundName = $_COOKIE['bgcolor'];
+    }
+    else if (isset($_POST['bgcolor'])) {
+        $backgroundName = $_POST['bgcolor'];
+    }
+    else {
+        $backgroundName = "gray";
+    } ?>
+
+<html>
+    <head><title>Save It</title></head>
+    <body bgcolor="<?= $backgroundName; ?>">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <p>Background color:
+                <select name="bgcolor">
+                    <option value="gray">Gray</option>
+                    <option value="white">White</option>
+                    <option value="black">Black</option>
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="red">Red</option>
+                </select></p>
+            <input type="submit" />
+</form>
+</body>
+</html>
