15426e0104b36ce05bebf5045731451e9c6c1ad0
renzosunico <r.sunico@klab.com>
2015-08-05 12:12:54 +0800

    Basic Array Class Implements Iterator Interface.

---
+++ BasicArray.php
@@ -0,0 +1,47 @@
+<?php
+
+class BasicArray implements Iterator
+{
+    private $position = 0;
+    private $array = array( "first", "second", "third" );
+
+    public function __construct()
+    {
+        $this->position = 0;
+    }
+
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    public function current()
+    {
+        return $this->array[$this->position];
+    }
+
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        $this->position += 1;
+    }
+
+    public function valid()
+    {
+        return isset($this->array[$this->position]);
+    }
+}
+
+    $basicArray = new BasicArray;
+
+    foreach ($basicArray as $value) {
+        echo "{$value} <br />";
+    }
+
+    foreach ($basicArray as $key => $value) {
+        echo "{$key} => {$value} <br />";
+    }
