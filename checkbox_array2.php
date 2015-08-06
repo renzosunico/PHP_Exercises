ff106b6bcb6c13b785c0b9b7960012f61b1c55c6
renzosunico <r.sunico@klab.com>
2015-08-06 08:13:51 +0800

    Bug fixed not displaying checkbox items.

--- checkbox_array2.php
+++ checkbox_array2.php
@@ -13,13 +13,11 @@
         function makeCheckboxes($name, $query, $options)
         {
             foreach ($options as $value => $label) {
-                $checked = in_array($value, $query) ? "checked" : '';
+                $checked = in_array($value, $query) ? "checked" : "";
+                echo "<input type=\"checkbox\" name=\"{$name}[]\" value=\"{$label}\" {$checked} />";
+                echo "{$label}<br />";
             }
         }
-
-        echo "<input type=\"checkbox\" name=\"{$name}\"
-        value=\"{$value}\" {$checked} />";
-        echo "{$label}<br />\n";
 
         $personalityAttributes = array(
             'perky' => "Perky",
