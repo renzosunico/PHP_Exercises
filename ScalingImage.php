81eb99b75081ecf9a3a1d41c43cf6b10d9d4bb91
renzosunico <r.sunico@klab.com>
2015-08-07 09:44:55 +0800

    Scaling Image Retrieves image from local directory and scale and pad it to new image.

---
+++ ScalingImage.php
@@ -0,0 +1,14 @@
+<?php
+    $source = imagecreatefrompng("logo.png");
+
+    $width = imagesx($source);
+    $height = imagesy($source);
+    $x = $width / 2;
+    $y = $height / 2;
+
+    $destination = imagecreatetruecolor($x, $y);
+    imagecopyresampled($destination, $source, 0, 0, 0, 0, $x, $y, $width, $height);
+    header("Content-Type: image/png");
+    imagepng($destination);
+
+?>
