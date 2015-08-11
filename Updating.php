ccd16704026a4d2d62e1b11761a92588dc437423
renzosunico <r.sunico@klab.com>
2015-08-11 11:48:37 +0800

    Updating Resources

---
+++ Updating.php
@@ -0,0 +1,26 @@
+<?php
+	$bookId = 'ProgrammingPHP';
+	$url = "http://example.com/api/books/{$bookId}";
+	
+	$data = json_encode(array('edition' => 3));
+	
+	$requestData = http_build_query($data, '', '&');
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	
+	$fh = fopen("php://memory", 'rw');
+	fwrite($fh, $requestData);
+	rewind($fh);
+	
+	curl_setopt($ch, CURLOPT_INFILE, $fh);
+	curl_setopt($ch, CURLOPT_INFILESIZE, mb_strlen($requestData));
+	curl_setopt($ch, CURLOPT_PUT, TRUE);
+	
+	$response = curl_exec($ch);
+	$resultinfo = curl_getinfo($ch);
+	
+	curl_close($ch);
+	fclose($fh);
+	
+	?>
